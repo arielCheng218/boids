7ef0957f0fd4554ec05f90f61e17a1626f98eed2
Ariel <Cheng>
2021-11-27 17:59:51 +0800

    direction matches movement

--- Boid.js
+++ Boid.js
@@ -4,7 +4,6 @@ class Boid {
   constructor() {
     const randomCoords = getRandomCoords()
     // position
-    this.pointsPositions = getInitialPoints(randomCoords)
     this.position = createVector(randomCoords[0], randomCoords[1])
     // velocity
     const randomStartingVelocity = getRandomNormalizedVector()
@@ -15,41 +14,13 @@ class Boid {
     this.position.add(this.velocity)
   }
 
-  // updatePointsPositions() {
-  //   for (var i = 0; i < this.pointsPositions.length; i++) {
-  //     if (i % 2 == 0) {
-  //       if ((this.pointsPositions[i] + this.velocity.x) < getViewWidth() && (this.pointsPositions[i] + this.velocity.x) > 0) {
-  //         this.pointsPositions[i] += this.velocity.x
-  //       } else {
-  //         this.pointsPositions = [0, 30, 10, 0, 20, 30]
-  //       }
-  //     } else {
-  //       if ((this.pointsPositions[i] + this.velocity.y) < getViewHeight() && (this.pointsPositions[i] + this.velocity.y) > 0) {
-  //         this.pointsPositions[i] += this.velocity.y
-  //       } else {
-  //         this.pointsPositions = [0, 30, 10, 0, 20, 30]
-  //       }
-  //     }
-  //   }
-  // }
-
   draw() {
+    // graphics
+    fill('#3faede');
+    // logic
+    this.updatePosition()
     translate(this.position.x, this.position.y)
     rotate(this.velocity.heading())
-    triangle(0, 30, 10, 0, 20, 30)
-    this.updatePosition()
-    this.updatePointsPositions()
+    triangle(-15, -8, -15, 8, 8, 0)
   }
-}
-
-function getInitialPoints(randomCoords) {
-  initialPoints = [0, 30, 10, 0, 20, 30]
-  for (var i = 0; i < initialPoints.length; i++) {
-    if (i % 2 == 0) {
-      initialPoints[i] += randomCoords[0]
-    } else {
-      initialPoints[i] += randomCoords[1]
-    }
-  }
-  return initialPoints
 }
