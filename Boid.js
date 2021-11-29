4cdfd1ba8c360feecaa83585ddafde1d26f369ec
Ariel <Cheng>
2021-11-29 17:54:15 +0800

    cleaned up code???

--- Boid.js
+++ Boid.js
@@ -1,32 +1,33 @@
 
 class Boid {
-
+  
   constructor() {
     const randomCoords = getRandomCoords()
     this.desiredSeparation = 20
     this.maxSpeed = 4
     this.position = createVector(randomCoords[0], randomCoords[1])
     const randomStartingVelocity = getRandomNormalizedVector()
-    this.velocity = createVector(randomStartingVelocity[0], randomStartingVelocity[1])
-    this.velocity.setMag(this.maxSpeed)
+    this.velocity = createVector(randomStartingVelocity[0], randomStartingVelocity[1]).setMag(this.maxSpeed)
     this.acceleration = createVector(0, 0)
+    this.maxForce = 0.1
   }
 
-  updatePosition() {
-    this.position.add(this.velocity)
+  drawArrow(base, vec, myColor) {
+    push();
+    stroke(myColor);
+    strokeWeight(3);
+    fill(myColor);
+    translate(base.x, base.y);
+    line(0, 0, vec.x, vec.y);
+    rotate(vec.heading());
+    let arrowSize = 7;
+    translate(vec.mag() - arrowSize, 0);
+    triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
+    pop();
   }
 
-  updateVelocity(boids) {
-    // separation
-    this.separate(boids)
-    // avoid obstacles
-    this.avoidWalls()
-    // alignment
-    // coherence
-  }
-
-  updateAcceleration() {
-    this.acceleration.mult(0)
+  applyForce(force) {
+    this.acceleration.add(force)
   }
 
   separate(boids) {
@@ -39,43 +40,59 @@ class Boid {
         count += 1
         // too close; move away from boid
         var steerAwayVector = boid.position.sub(this.position)
+        this.drawArrow(steerAwayVector, this.position, '#eb9ac1')
         steerAwayVector.normalize()
         steerAwayVector.div(distance)
         desiredVelocity.add(steerAwayVector)
       }
     }
     if (count > 0) {
-      desiredVelocity.div(count)
-      desiredVelocity.normalize()
-      desiredVelocity.mult(this.maxSpeed)
-      this.acceleration = desiredVelocity.sub(this.velocity)
-      this.acceleration.limit(1)
+      desiredVelocity.setMag(this.maxSpeed)
+      var steer = desiredVelocity.sub(this.velocity)
+      steer.limit(this.maxForce)
+      this.applyForce(steer)
     }
   }
 
   avoidWalls() {
-    if (this.position.x <= 0 || this.position.y <= 0 || this.position.x > getViewWidth() - 20 || this.position.y > getViewHeight() - 100) {
-      this.velocity.mult(-1)
+    if (this.position.x > getViewWidth()) {
+      this.position.x = getViewWidth()
+      this.velocity.x *= -1
+    } else if (this.position.x < 0) {
+      this.velocity.x *= -1
+      this.position.x = 0
+    }
+    if (this.position.y > getViewHeight() - 100) {
+      this.velocity.y *= -1
+      this.position.y = getViewHeight() - 100
+    } else if (this.position.y < 0) {
+      this.velocity.y *= -1
+      this.position.y = 0
     }
   }
 
   update(boids) {
+    // update velocity
+    this.separate(boids)
+    this.avoidWalls()
     this.velocity.add(this.acceleration)
-    this.updateVelocity(boids)
+    this.velocity.limit(this.maxSpeed)
+    // update position
     this.position.add(this.velocity)
+    // clear acceleration
     this.acceleration.mult(0)
   }
 
   draw(boids) {
     // graphics
     fill('#3faede')
+    this.update(boids)
     push()
     // logic
     translate(this.position.x, this.position.y)
     rotate(this.velocity.heading())
     triangle(-10, -5, -10, 5, 5, 0)
     pop()
-    this.update(boids)
   }
 
 }
